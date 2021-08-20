7f1c74e18e781eb25dcc046529b3e620f0fbf8e8
JosePasini <josepasini.17@gmail.com>
2021-08-20 15:17:51 -0300

    Lo mismo, con la librería LOMBOK en la clase Articulo, etiquetas @getters, @setters, @ToString, @@NoArgsConstructor @AllArgsConstructor @RequiredArgsConstructor

--- src/main/java/entidades/Articulo.java
+++ src/main/java/entidades/Articulo.java
@@ -16,8 +16,15 @@ import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import lombok.*;
+
 @Entity
 @Table(name = "articulo")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Articulo implements Serializable{
 
 	private static final long serialVersionUID = 1L;
@@ -52,56 +59,18 @@ public class Articulo implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
+	
+	@Column(name = "precio")
+	@NonNull private int precio;
+	
 	@Column(name = "cantidad")
 	private int cantidad;
 	
 	@Column(name = "denominacion")
-	private String denominacion;
+	@NonNull private String denominacion;
 
-	@Column(name = "precio")
-	private int precio;
 
-	public long getId() {
-		return id;
-	}
 
-	public void setId(long id) {
-		this.id = id;
-	}
 
-	public int getCantidad() {
-		return cantidad;
-	}
-
-	public void setCantidad(int cantidad) {
-		this.cantidad = cantidad;
-	}
-
-	public String getDenominacion() {
-		return denominacion;
-	}
-
-	public void setDenominacion(String denominacion) {
-		this.denominacion = denominacion;
-	}
-
-	public int getPrecio() {
-		return precio;
-	}
-
-	public void setPrecio(int precio) {
-		this.precio = precio;
-	}
-
-	public Articulo(int cantidad, String denominacion, int precio) {
-		this.cantidad = cantidad;
-		this.denominacion = denominacion;
-		this.precio = precio;
-	}
-	public Articulo(int cantidad, String denominacion) {
-		this.cantidad = cantidad;
-		this.denominacion = denominacion;
-	}
-	
 	
 }
